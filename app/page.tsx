3b970a3e17fbe6e4333d44912c622a2b1db07008
Morpheus Zein <morpheus.zein@irawan.id>
2024-03-25 01:16:53 +0700

    yep

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,9 @@
-'use client'
-import { useEditor, Editor, EditorContent, Content, } from '@tiptap/react';
-import { Underline as lineUnder } from '@tiptap/extension-underline';
-import { StarterKit } from '@tiptap/starter-kit';
-import Placeholder from '@tiptap/extension-placeholder';
-import TipTap from './Rcomponents/TipTap';
-
 
 export default function Home() {
-  const editor = new Editor({
-    extensions: [
-      StarterKit,
-      lineUnder,
-      Placeholder
-    ],
-    editorProps: {
-      attributes: {
-        class: 'border'
-      }
-    }
-  })
+
   return (
     <div className="flex flex-col items-center">
-      <TipTap editor={editor} />
+      <h1>cihuy</h1>
     </div>
   )
 }
